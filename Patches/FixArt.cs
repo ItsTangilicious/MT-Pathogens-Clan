0f8a852ea0858fc3202679d456cf174e53c3500f
ItsTangilicious <itstangiliciousgaming@gmail.com>
2024-05-12 22:01:05 -0400

    Add files via upload

--- Patches/FixArt.cs
+++ Patches/FixArt.cs
@@ -7,7 +7,8 @@ using Trainworks.Managers;
 
 namespace Patches
 {
-    public static class FixArt
+    //I am not good with the code and could not get this to work for Recombinant Virus spawns, so see the CardEffectLoadRecombinantArt for how I made it work.
+   /* public static class FixArt
     {
         public static int cardsToLoad;
 
@@ -24,18 +25,22 @@ namespace Patches
 
         public static void TryYetAnotherFix(List<CardData> cards, LoadingScreen.DisplayStyle displayStyle = LoadingScreen.DisplayStyle.FullScreen, Action doneCallbak = null)
         {
-            SaveManager saveManager = ProviderManager.SaveManager;
+            var saveManager = Trainworks.Managers.ProviderManager.SaveManager;
             CardState card = new CardState();
 
             cardsToLoad += cards.Count;
             foreach (CardData virus in cards)
             {
-                LoadingScreen.AddTask(new LoadAdditionalCards(CustomCardManager.GetCardDataByID(virus.GetID()), loadSpawnedCharacters: true, displayStyle, delegate
+                LoadingScreen.AddTask
+                    (
+                    new LoadAdditionalCards
+                    (
+                        CustomCardManager.GetCardDataByID(virus.GetID()), loadSpawnedCharacters: true, displayStyle, delegate
                 {
                     cardsToLoad--;
                 }));
             }
         }
         
-    }
+    }*/
 }
