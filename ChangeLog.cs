db0b483c3190d22658bf9311088b2280ac3813a3
ItsTangilicious <itstangiliciousgaming@gmail.com>
2024-05-18 13:19:22 -0400

    Add files via upload

--- ChangeLog.cs
+++ ChangeLog.cs
@@ -6,12 +6,23 @@ namespace Test_Bounce
 {
     internal class ChangeLog
     {
-       /*
-        * No actual coding here, just a change log.
-        * 
-        *May 17, 2024
-        *Added Status Icons, Clan Banners, Clan Cardback, Clan Symbol assets.
-        *
-        */
+        /*
+         * No actual coding here, just a change log.
+         * 
+         *May 17, 2024
+         *Added Status Icons, Clan Banners, Clan Cardback, Clan Symbol assets.
+         *
+         *May 18, 2024
+         *Carrier Immunologist stat gain reduced from 3/3; 5/5; 10/10 → 2/2; 4/4; 8/8
+         *Carrier Bioengineer stat gain reduced from 10/10; 25/25; 50/50 → 10/5; 20/10; 40/20
+         *Added RelicActivatedKey to Aerosolizer, Growth Chamber, Research Notebook
+         *Moved Aeroslizer over to Localization CSV
+         *Reduced Evolving Resistance play from Armor 10 → Armor 5
+         *Reduced Evolving Resistance 2A play from Armor 10, Rage 5 → Armor 5, Rage 3
+         *Reduced Evolving Resistance 2B play from Armor 10 → Armor 5
+         *Reduced Evolving Resistance 3A play from Armor 10, Rage 5 → Armor 5, Rage 3
+         *Reduced Evolving Resistance 3B play from Armor 12, Rage 6 → Armor 6, Rage 3
+         *Reduced Evolving Resistance 3C play from Armor 10 → Armor 5
+         */
     }
 }
