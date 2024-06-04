decaf40286fa5f98df1fc46aa7761b111ea122c3
ItsTangilicious <itstangiliciousgaming@gmail.com>
2024-06-03 22:44:41 -0400

    Add files via upload

---
+++ CustomEffectsPathogens/StatusEffectReplicateStackable.cs
@@ -0,0 +1,65 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Trainworks.ConstantsV2;
+using Trainworks.BuildersV2;
+using Trainworks.Managers;
+using static StatusEffectState;
+
+
+
+
+namespace CustomEffectsPathogens
+{
+    public class StatusEffectReplicateStackable : StatusEffectState
+        //use this for Replicate cards that can be doublestacked
+    {
+        public static StatusEffectData data;
+        public const string statusID = "status_replicatestackable";
+        //public static List<CardState> cardsTriggeredOn = new List<CardState>() { };
+
+        public static void Build()
+        {
+            data = new StatusEffectDataBuilder()
+            {
+                StatusEffectStateType = typeof(StatusEffectReplicateStackable),
+                StatusID = statusID,
+                IsStackable = true,
+                IconPath = "AssetsAll/ClanAssets/ReplicateLarge.png",
+                TooltipIconPath = "AssetsAll/ClanAssets/ReplicateSmall.png",
+                TriggerStage = StatusEffectData.TriggerStage.None,
+                DisplayCategory = StatusEffectData.DisplayCategory.Positive,
+                ShowStackCount = false,
+                RemoveAtEndOfTurn = false,
+                RemoveWhenTriggered = true,
+
+                //Description = "Gains Endless. Loses Replicate when this unit dies.",
+                ParamInt = 1,
+
+
+            }.Build();
+            List<StatusEffectData.TriggerStage> triggerStages = new List<StatusEffectData.TriggerStage>
+            {
+                StatusEffectData.TriggerStage.OnPostEaten
+            };
+            AccessTools.Field(typeof(StatusEffectData), "additionalTriggerStages").SetValue(data, triggerStages);
+
+        }
+
+        //This is applying in Preview Mode...test if ProviderManager fixes issue?
+        public override void OnStacksAdded(CharacterState character, int numStacksAdded)
+        {
+
+            if (numStacksAdded > 0 && !ProviderManager.SaveManager.PreviewMode)
+            {
+                character.AddStatusEffect(VanillaStatusEffectIDs.Endless, 1);
+            }
+
+        }
+
+    }
+}
+
+
